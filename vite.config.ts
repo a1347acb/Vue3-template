9c51b9891fe1543aea8eca2509d134e0f93c194f
陈龙 <chen_teacher@163.com>
2024-01-03 21:17:51 +0800

    chore: 配置@符号

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
+import * as path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -56,4 +57,9 @@ export default defineConfig({
       dts: './components.d.ts'
     })
   ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  }
 })

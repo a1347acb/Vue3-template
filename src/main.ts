53555cea125c1822b7cc4abc2ec0795c9ee4d068
陈龙 <chen_teacher@163.com>
2024-01-03 21:07:53 +0800

    chore: 安装/配置unplugin-auto-import

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,5 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-import { initMicroApp } from './microApp'
 
-initMicroApp()
 createApp(App).mount('#app')
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,48 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    AutoImport({
+      // targets to transform
+      include: [
+        /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
+        /\.vue$/,
+        /\.vue\?vue/, // .vue
+        /\.md$/, // .md
+      ],
+
+      // global imports to register
+      imports: [
+        // presets
+        'vue',
+      ],
+      // Enable auto import by filename for default module exports under directories
+      defaultExportByFilename: false,
+
+      // Auto import for module exports under directories
+      // by default it only scan one level of modules under the directory
+      dirs: [
+        // './hooks',
+        // './composables' // only root modules
+        // './composables/**', // all nested modules
+        // ...
+      ],
+
+      // Filepath to generate corresponding .d.ts file.
+      // Defaults to './auto-imports.d.ts' when `typescript` is installed locally.
+      // Set `false` to disable.
+      dts: './auto-imports.d.ts',
+
+      // Auto import inside Vue template
+      // see https://github.com/unjs/unimport/pull/15 and https://github.com/unjs/unimport/pull/72
+      vueTemplate: false,
+
+      // Inject the imports at the end of other imports
+      injectAtEnd: true,
+    })
+  ],
 })
